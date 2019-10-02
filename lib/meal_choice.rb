a0a7f7157ea6a8ffb04c32bcaf1c8c83b263a353
Hussain Ahmed <haz6798@icloud.com>
2019-10-02 03:24:31 +0000

    Automatically backed up by Learn

---
+++ lib/meal_choice.rb
@@ -0,0 +1,12 @@
+def meal_choice(veg1, veg2, protein = "meat")
+  
+  puts "what a nutritous meal"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
+  
+end
+
+veg1 = "Carrot"
+veg2 = "Fish"
+
+ puts meal_choice
+ 
